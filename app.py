5936a4bbbcf430aa61fd7b80be0ae293fb3d325c
alocse <83795773+alocse@users.noreply.github.com>
2021-06-20 12:23:41 -0300

    Update app.py
    e

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def get():
 
 @app.route("/calculateTRI", methods=["POST"])
 def calculate():
-    #body = request.get_json()
-    #tri = execute(body)
-    return "ola"
+    body = request.get_json()
+    tri = execute(body)
+    return tri
 
 if __name__ == "__main__":
     app.run()
