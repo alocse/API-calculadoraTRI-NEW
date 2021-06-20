0ea2ab65bfa84f39f75c94a03df41f22ed09d707
alocse <83795773+alocse@users.noreply.github.com>
2021-06-19 21:28:43 -0300

    Update app.py
    dd

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from flask import Flask, request
 
 from main import execute
 
-
 app = Flask("alocse")
 
 @app.route("/teste", methods=["GET"])
 def get():
     return {"ok": "mundo1"}
+
+@app.route("/home")
+def home():
+    return "Flask API get endpoint running"
 
 app.run()
