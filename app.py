8318bae80dfcd642957776e86fca2c0d17810862
alocse <83795773+alocse@users.noreply.github.com>
2021-06-19 21:40:30 -0300

    Update app.py
    sss

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = flask.Flask(__name__)
 def home():
     return "Flask API get endpoint running"
 
+
+@app.route("/teste", methods=["GET"])
+def get():
+    return {"ok": "mundo1"}
+
 if __name__ == "__main__":
     app.run()
