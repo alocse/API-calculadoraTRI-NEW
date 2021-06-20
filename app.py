93e838003449bd35531e456e52d42c82aa3f6056
alocse <83795773+alocse@users.noreply.github.com>
2021-06-19 21:34:39 -0300

    Update app.py
    www

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
 import Flask
 
-app = Flask("alocse")
+app = flask.Flask(__name__)
 
 
 @app.route("/home")
 def home():
     return "Flask API get endpoint running"
 
-app.run()
+if __name__ == "__main__":
+    app.run()
